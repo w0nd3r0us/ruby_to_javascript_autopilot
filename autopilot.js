bdae06f002b69786c0822a401ea734bf11a37f1b
w0nd3r0us <steven.grewal@gmail.com>
2018-07-11 19:54:02 -0400

    can show gas

--- autopilot.js
+++ autopilot.js
@@ -18,3 +18,24 @@ function addPassengers(passNum, carObj) {
   carObj.passenger = passNum;
   carObj.gas -= 10;
 };
+
+function getDestination(carObj) {
+  if (carObj.city === 'Toronto') {
+    var value = 'mississauga';
+  } else if (carObj.city === 'mississauga') {
+    var value = 'London';
+  } else if (carObj.city === 'London') {
+    var value = 'Toronto';
+  };
+  return value
+}
+
+function fillUpGas(carObj) {
+  prevGas = carObj.gas;
+  carObj.gas = 100;
+  console.log("Filled up to " + carObj.gas + " gas from " + prevGas);
+};
+
+function showGas(carObj) {
+  console.log('Gas Level is:' + carObj.gas + " %.");
+};
