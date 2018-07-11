62fbd7ac046d68cebdf429c6cff4974dff40191e
w0nd3r0us <steven.grewal@gmail.com>
2018-07-11 19:50:31 -0400

    must needed commit

--- autopilot.js
+++ autopilot.js
@@ -1,5 +1,20 @@
-var getNewCar {
-  city: 'toronto',
-  passengers: 0,
-  gas: 100,
-}
+
+function getNewCar(city, passengers) {
+  var newCar = {
+    'city': city,
+    'passengers': passengers,
+    'gas': 100,
+  }
+  return newCar
+};
+
+var carArray = [];
+function addCar(car) {
+  carArray.push(car);
+  console.log("Adding new car to fleet. Fleet size is now" +carArray.length ".");
+};
+
+function addPassengers(passNum, carObj) {
+  carObj.passenger = passNum;
+  carObj.gas -= 10;
+};
